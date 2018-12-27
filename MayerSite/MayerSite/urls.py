c4ee4ebdacdee8bbec0c1e2bb9fab721b6e3ad74
Mayer Cloud <mayerdontwasteyourlife201612@outlook.com>
2018-12-27 10:41:27 +0800

    新增 PoorIT

--- MayerSite/MayerSite/urls.py
+++ MayerSite/MayerSite/urls.py
@@ -3,12 +3,15 @@ Definition of urls for MayerSite.
 """
 
 from django.conf.urls import include, url
+import PoorIT.views
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = [
+    url(r'^$', PoorIT.views.index, name='index'),
+    url(r'^home$', PoorIT.views.index, name='home'),
     # Examples:
     # url(r'^$', MayerSite.views.home, name='home'),
     # url(r'^MayerSite/', include('MayerSite.MayerSite.urls')),
---
+++ MayerSite/PoorIT/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PoorITConfig(AppConfig):
+    name = 'PoorIT'
